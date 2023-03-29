8160cf47e4cdd789aeb79dfc592dcbde5dcaf5df
vinyashegde <vinyasvasanthegde7@gmail.com>
2023-03-30 01:57:49 +0530

    user details saved in database

--- app.js
+++ app.js
@@ -8,19 +8,36 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "445260306945",
     appId: "1:445260306945:web:cb42789eee6467d52fbe00"
   });
-  const db = firebaseApp.firestore();
+  const database = firebaseApp.database();
   const auth = firebaseApp.auth();
   
   //sign up
   const signUp=()=>{
     const email = document.getElementById("email").value;
+    const fullname = document.getElementById("fullname").value;
+    const location = document.getElementById("location").value;
     const password = document.getElementById("password").value;
     console.log(email,password);
     //firebase signup code
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((result) => {
       // Signed in 
-    //   document.write("Your Signed up")
+      //document.write("Your Signed up")
+      var user = auth.currentUser
+
+      //Add this user to firebase database
+      var database_ref = database.ref()
+
+      // Create User data
+      var user_data ={
+        email:email,
+        fullname:fullname,
+        location: location,
+        last_login : Date.now()
+      }
+
+      database_ref.child('users/' + user.uid).set(user_data)
+
       console.log(result)
       M.toast({html: `Registered Successfully`,classes:"green"})
       // ...
